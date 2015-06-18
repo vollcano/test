2dd415c7f35e1aae72f8c4452ca4357fb2776b46
ouyangnan <ocandy@qq.com>
2015-06-18 16:18:22 +0800

    初步实现加法计算器 要点：label.text提取之后通过.intvalue转化成int int通过stringwithformat转化成string

--- githubtest/githubtest/ViewController.m
+++ githubtest/githubtest/ViewController.m
@@ -9,10 +9,19 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *num1;
+@property (weak, nonatomic) IBOutlet UITextField *num2;
+@property (weak, nonatomic) IBOutlet UILabel *result;
 
 @end
 
 @implementation ViewController
+- (IBAction)compute:(id)sender {
+    int s1 = self.num1.text.intValue + self.num2.text.intValue;
+    self.result.text = [NSString stringWithFormat:@"%d",s1];
+    [self.view endEditing:YES];
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
