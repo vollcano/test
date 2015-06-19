0eaef0ba03b9456755aef6627c7d0e6762ea0b88
ouyangnan <ocandy@qq.com>
2015-06-19 13:16:56 +0800

    use cgaffinetransform to scale/translate/rotate

--- githubtest/githubtest/ViewController.m
+++ githubtest/githubtest/ViewController.m
@@ -24,42 +24,55 @@ typedef enum{
 @end
 @implementation ViewController
 - (IBAction)move:(UIButton *)sender {
-    CGRect frame = self.icon.frame;
-    switch (sender.tag) {
-        case kMovingDirUp:
-            frame.origin.y -= kMovingDelta;
-            break;
-        case kMovingDirBottom:
-            frame.origin.y += kMovingDelta;
-            break;
-        case kMovingDirLeft:
-            frame.origin.x -= kMovingDelta;
-            break;
-        case kMovingDirRight:
-            frame.origin.x += kMovingDelta;
-            break;
-            
-        default:
-            break;
+//    CGRect frame = self.icon.frame;
+//    switch (sender.tag) {
+//        case kMovingDirUp:
+//            frame.origin.y -= kMovingDelta;
+//            break;
+//        case kMovingDirBottom:
+//            frame.origin.y += kMovingDelta;
+//            break;
+//        case kMovingDirLeft:
+//            frame.origin.x -= kMovingDelta;
+//            break;
+//        case kMovingDirRight:
+//            frame.origin.x += kMovingDelta;
+//            break;
+//            
+//        default:
+//            break;
+//    }
+//    self.icon.frame = frame;
+//    NSLog(@"%@",NSStringFromCGRect(self.icon.frame));
+    CGFloat dx = 0,dy = 0;
+    if (sender.tag == kMovingDirUp || sender.tag == kMovingDirBottom) {
+        dy = (sender.tag == kMovingDirUp ? -kMovingDelta :kMovingDelta);
+    }else{
+        dx = (sender.tag == kMovingDirLeft ? -kMovingDelta :kMovingDelta);
     }
-    self.icon.frame = frame;
+
+    self.icon.transform = CGAffineTransformTranslate(self.icon.transform, dx, dy);
+
+    NSLog(@"%@",NSStringFromCGAffineTransform(self.icon.transform));
     
 }
 
 - (IBAction)zoom:(UIButton *)sender {
-    CGRect bounds = self.icon.bounds;
-    if (sender.tag ) {
-        bounds.size.width += kMovingDelta;
-        bounds.size.height += kMovingDelta;
-    }else {
-        bounds.size.width -= kMovingDelta;
-        bounds.size.height -= kMovingDelta;
-    }
-    [UIView beginAnimations:nil context:nil];
-    [UIView setAnimationDuration:1.0];
-    self.icon.bounds = bounds;
-    [UIView commitAnimations];
+    CGFloat scale = sender.tag ? 1.2 :0.8;
+    self.icon.transform = CGAffineTransformScale(self.icon.transform, scale, scale);
+    NSLog(@"%@",NSStringFromCGAffineTransform(self.icon.transform));
+
     
+}
+
+- (IBAction)rotate:(UIButton *)sender {
+    
+    CGFloat angle =( sender.tag) ? -M_PI : M_PI;
+    [UIView beginAnimations:nil context:nil];
+    self.icon.transform = CGAffineTransformRotate(self.icon.transform, angle);
+    [UIView commitAnimations];
+    NSLog(@"%@",NSStringFromCGAffineTransform(self.icon.transform));
+    NSLog(@"%@",NSStringFromCGRect(self.icon.frame));
 }
 
 
