0c1cb242904572c441204e841baccf5343ce2c25
ouyangnan <ocandy@qq.com>
2015-06-19 15:25:18 +0800

    creating and adding button by hand

--- githubtest/githubtest/ViewController.m
+++ githubtest/githubtest/ViewController.m
@@ -17,12 +17,25 @@ typedef enum{
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *icon;
-
-
-
-
 @end
+
 @implementation ViewController
+- (void)viewDidLoad{
+    UIButton *b1 = [UIButton buttonWithType:UIButtonTypeInfoDark];
+    b1.center = CGPointMake(200, 200);
+    //[b1 setBackgroundColor:[UIColor redColor]];
+    [self.view addSubview:b1];
+    UIButton *btn = [[UIButton alloc]initWithFrame:CGRectMake(10, 10, 100, 100)];
+    [btn setTitle:@"touch me" forState:UIControlStateNormal];
+    [btn setTitle:@"why?" forState:UIControlStateHighlighted];
+    [btn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor blueColor] forState:UIControlStateHighlighted];
+    [btn setBackgroundImage:[UIImage imageNamed:@"pic1"] forState:UIControlStateNormal];
+    [btn setBackgroundImage:[UIImage imageNamed:@"pic2"] forState:UIControlStateHighlighted];
+    [btn setContentVerticalAlignment:UIControlContentVerticalAlignmentBottom];
+    [self.view addSubview:btn];
+    
+}
 - (IBAction)move:(UIButton *)sender {
 //    CGRect frame = self.icon.frame;
 //    switch (sender.tag) {
