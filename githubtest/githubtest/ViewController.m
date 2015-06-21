816dfa74c3dbf1cda2af0a0eedd1d3646dd522d9
ouyangnan <ocandy@qq.com>
2015-06-21 12:50:43 +0800

    the empty project

--- githubtest/githubtest/ViewController.m
+++ githubtest/githubtest/ViewController.m
@@ -7,44 +7,15 @@
 //
 
 #import "ViewController.h"
-typedef enum{
-    kMovingDirUp = 10,
-    kMovingDirBottom,
-    kMovingDirLeft,
-    kMovingDirRight,
-    }kMovingDir;
-#define kMovingDelta 20.0f
+
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UIButton *icon;
 
 
 
 
 @end
 @implementation ViewController
-- (IBAction)move:(UIButton *)sender {
-    CGRect frame = self.icon.frame;
-    switch (sender.tag) {
-        case kMovingDirUp:
-            frame.origin.y -= kMovingDelta;
-            break;
-        case kMovingDirBottom:
-            frame.origin.y += kMovingDelta;
-            break;
-        case kMovingDirLeft:
-            frame.origin.x -= kMovingDelta;
-            break;
-        case kMovingDirRight:
-            frame.origin.x += kMovingDelta;
-            break;
-            
-        default:
-            break;
-    }
-    self.icon.frame = frame;
-    
-}
 
 
 
